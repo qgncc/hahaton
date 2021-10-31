c5e973265d4f627f3d09f0a9a3610d04b89f11ac
evtn <k01419q45@ya.ru>
2021-10-31 11:44:11 +0700

    yes

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,27 +1,20 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import {UilAngleRightB} from "@iconscout/react-unicons";
+import { UilAngleRightB } from "@iconscout/react-unicons";
 
-function Accordion(props) {
+function Accordion({title, id, value, children, ...props}) {
     const [isOpen, setOpen] = useState(true);
 
-    return(
-    <React.Fragment>
-        <div className="result t-result accordion-title" data-accordion_id = {props.id} onClick={()=>{
-                if(isOpen)
-                    setOpen(false);
-                else
-                    setOpen(true)
-            }
-        }>
-            <span className="text"><h4><UilAngleRightB className = "icon"/>{props.title}</h4></span>
-            <span className="text"><h5>{props.value}</h5></span>
-
-        </div>
-        <div id = {props.id}  >
-            {isOpen ? props.children : null}
-        </div>
-    </React.Fragment>
+    return (
+        <React.Fragment>
+            <div className="result t-result accordion-title" data-accordion_id={id} onClick={() => setOpen(!isOpen)}>
+                <span className="text"><h4><UilAngleRightB className="icon"/>{title}</h4></span>
+                <span className="text"><h5>{value}</h5></span>
+            </div>
+            <div id={id}>
+                {isOpen ? children : null}
+            </div>
+        </React.Fragment>
     );
 }
 
--- src/App.js
+++ src/App.js
@@ -8,68 +8,52 @@ import { UilAngleRightB } from '@iconscout/react-unicons'
 import './App.css';
 
 
-
-
-
-
 function Input () {
     return (
-        <input style = {{textAlign:'right'}} className="input t-input" type="numeric" />
+        <input style={{textAlign:'right'}} className="input t-input" type="numeric" />
     )
 }
-function Currency({values,label, selected, ...props}) {
+function Currency({values, label, selected, ...props}) {
     return(
-            <select id = "currency" className= "select t-select ">
-
-                    {
-                        values.map(e =>
-                            {
-                                return(
-                                    <option selected={selected === e.value} value={e.value}>{e.name}</option>
-
-                                );
-                            }
-                        )
-
-                    }
-            </select>
+        <select id="currency" className="select t-select">
+            {
+                values.map(
+                    e => <option selected={selected === e.value} value={e.value}>{e.name}</option>
+                )
+            }
+        </select>
     );
 }
 
 function App() {
     const values = [
-        {name:"BTC", value:10},
-        {name:"ETH", value:20},
-        {name:"ETC", value:30},
-        {name:"LTS", value:40},
-        {name:"USD", value:50},
+        {name: "BTC", value:10},
+        {name: "ETH", value:20},
+        {name: "ETC", value:30},
+        {name: "LTС", value:40},
+        {name: "USDT", value:50},
 
     ];
 
-
-
-  return (
-
+    return (
         <div className="wrapper t-wrapper">
             <div className="card t-card">
-                <h1 className="title t-title">Neptune foundation</h1>
+                <h1 className="title t-title">Neptune Foundation</h1>
                 <div className="form">
-                    <Input />
-                        <Currency values = {values} label = "$" selected = {10}/>
+                    <Input/>
+                        <Currency values = {values} label="$" selected={10}/>
                         <span className="text">/</span>
-                        <Currency values = {values} label = "$" selected = {50}/>
-                    <button className= "submit t-submit" type="submit" value = "Calc"><UilArrowRight/></button>
+                        <Currency values={values} label="$" selected={50}/>
+                    <button className= "submit t-submit" type="submit" value="Calc"><UilArrowRight/></button>
                 </div>
                 <div className="result-wrapper" >
-                    <Accordion value = "2137124" title="Рыночная цена" id="accordion-1">loredasihjsghfghuajfodahgjhajdhf</Accordion>
-                    <Accordion value = "2137124" title="Продажа" id="accordion-1">loredasihjsghfghuajfodahgjhajdhf</Accordion>
-                    <Accordion value = "2137124" title="Покупка" id="accordion-1">loredasihjsghfghuajfodahgjhajdhf</Accordion>
-
+                    <Accordion value="2137124" title="Рыночная цена" id="accordion-1">loredasihjsghfghuajfodahgjhajdhf</Accordion>
+                    <Accordion value="2137124" title="Продажа" id="accordion-1">loredasihjsghfghuajfodahgjhajdhf</Accordion>
+                    <Accordion value="2137124" title="Покупка" id="accordion-1">loredasihjsghfghuajfodahgjhajdhf</Accordion>
                 </div>
             </div>
         </div>
-
-  );
+    );
 }
 
 
